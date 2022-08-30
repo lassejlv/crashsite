10e954f3eee1e7aa25b0ef84ed99131e4bc8183b
lassev05 <vestergaardlasse2@gmail.com>
2022-08-30 21:16:02 +0200

    Website done.

---
+++ servemon.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  projectName: "crashsite",
+  port: 3000, // Port to use
+  directory: "./src", // Directory to use
+  pkgManager: "servemon", // Package manager you want to use
+  watch: true, // Watch for changes
+  open: false, // Open the browser
+  fileExplore: false, // Enable file explorer
+  logger: false, // Enable logger
+};
---
+++ src/index.js
@@ -0,0 +1,9 @@
+let youDead = document.getElementById("youDead");
+
+youDead.addEventListener("click", () => {
+  alert(
+    "You just killed the website, you are a murder right now? 💀 We told you not to click the button "
+  );
+
+  while (true) {}
+});
